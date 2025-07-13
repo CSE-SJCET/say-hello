1c848293dc90863c2c48041b4077020faeaeaba6
Sidharth-creator <sidharths2028@cs.sjcetpalai.ac.in>
2025-07-13 14:51:16 +0530

    Update HelloName.java

--- HelloName.java
+++ HelloName.java
@@ -7,6 +7,7 @@ public class HelloName {
 
         System.out.println("What is your name?");
         line = scanner.nextLine();
+        System.out.println("Hello"+"\t"+line);
 
         // Put the correct code here!
     }
